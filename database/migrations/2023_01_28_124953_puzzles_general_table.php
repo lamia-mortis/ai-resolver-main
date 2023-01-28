472963a09e1dc868e547dfa1889ab00cde949e3f
Kristina Lazorenko <Liama.Mirtos@gmail.com>
2023-01-28 15:33:12 +0100

    [00003-0] Implementing MongoDB

--- database/migrations/2023_01_28_124953_puzzles_general_table.php
+++ database/migrations/2023_01_28_124953_puzzles_general_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,14 +15,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('puzzles_general', function (Blueprint $table) {
+            $table->string('key')->unique();
+            $table->string('name');
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('puzzles_general');
     }
 };
