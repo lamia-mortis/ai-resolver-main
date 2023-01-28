39f3dd8ec64c73e0e0bc2dbcdf538a1f5aa4d93b
MrDrProfessorPatrick <vip.salm2@gmail.com>
2023-01-29 00:34:36 +0200

    [00004-0] Creating sudoku board. Adding validation for board. Styling valid and invalid cells. Sending sudoku data to the server

--- resources/js/Components/Pages/Sudoku/SudokuIndex.tsx
+++ resources/js/Components/Pages/Sudoku/SudokuIndex.tsx
@@ -1,12 +1,23 @@
 'use strict';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../../Layout/Layout';
+import { Board } from './Board/Board';
 
 export default function SudokuIndex() {
+  const [message, setMessage] = useState<string>('');
+  const [messageVariant, setMessageVariant] = useState<string>('');
+
   return (
     <Layout>
-      <div className='text-white'>SUDOKU</div>
+      <div className='text-white'>
+        <Board
+          message={message}
+          setMessage={setMessage}
+          messageVariant={messageVariant}
+          setMessageVariant={setMessageVariant}
+        />
+      </div>
     </Layout>
   );
 }
